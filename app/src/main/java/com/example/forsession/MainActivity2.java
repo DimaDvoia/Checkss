26e7022b3b16ac19d62d01b041b60e149f80b0c5
ДимаДвоянов <dcoiuuu@mail.ru>
2023-12-26 03:13:14 +0700

    Third commit

--- app/src/main/java/com/example/forsession/MainActivity2.java
+++ app/src/main/java/com/example/forsession/MainActivity2.java
@@ -4,24 +4,63 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
 
-    TextView loginTextVie;
+    TextView loginTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
-        loginTextVie = findViewById(R.id.HelloLogin);
+        loginTextView = findViewById(R.id.HelloLogin);
 
         Intent intent = getIntent();
 
         String login = intent.getStringExtra("LOGIN");
 
-        loginTextVie.setText("Привет:" + "" + login);
+        loginTextView.setText("Привет:" + "" + login);
+
+        Button showUsersButton = findViewById(R.id.button);
+        showUsersButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MyDatabaseHelper dbHelper = new MyDatabaseHelper(getApplicationContext());
+                SQLiteDatabase db = dbHelper.getReadableDatabase();
+
+                String[] projection = {
+                        MyDatabaseHelper.COLUMN_LOGIN,
+                        MyDatabaseHelper.COLUMN_PASSWORD
+                };
+
+                Cursor cursor = db.query(
+                        MyDatabaseHelper.TABLE_USERS,
+                        projection,
+                        null,
+                        null,
+                        null,
+                        null,
+                        null
+                );
+
+                while (cursor.moveToNext()) {
+                    String login = cursor.getString(cursor.getColumnIndexOrThrow(MyDatabaseHelper.COLUMN_LOGIN));
+                    String password = cursor.getString(cursor.getColumnIndexOrThrow(MyDatabaseHelper.COLUMN_PASSWORD));
+
+                    Log.d("User", "Login: " + login + ", Password: " + password);
+                }
+
+                cursor.close();
+                db.close();
+            }
+        });
     }
 }
